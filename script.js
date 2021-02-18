7968607a62043840e90fcce9759e7a5d630ad428
JJ <jonathanjablonski94@gmail.com>
2021-02-18 12:36:54 -0600

    Adding files to main branch

---
+++ script.js
@@ -0,0 +1,128 @@
+// Array of special characters to be included in password
+var specialCharacters = ["!", "@", "$", "%"];
+
+// Array of numeric characters to be included in password
+var numericCharacters =["1","2","3","4"];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = ["d","e","f","g"];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = ["H","I","J","K"];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  alert()
+  // Variable to store length of password from user input
+  var passLength = ;
+
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (isNaN(passLength) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+
+  // Variable to store boolean regarding the inclusion of special characters
+  var specialCharactersConfirm = ;
+
+  // Variable to store boolean regarding the inclusion of numeric characters
+  var numericCharactersConfirm = ;
+
+  // Variable to store boolean regarding the inclusion of lowercase characters
+  var lowerCasedCharactersConfirm = ;
+
+  // Variable to store boolean regarding the inclusion of uppercase characters
+  var upperCasedCharactersConfirm = ;
+
+  // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+  if () {
+    alert('Must select at least one character type');
+    return;
+  }
+
+  // Object to store user input variables
+  var passwordOptions = {
+    
+  };
+  // Return the options object as the exported value of this function
+  return passwordOptions;
+}
+
+// Function for getting a random element from an array
+function getRandomElement(arr) {
+  // Get a random number based on the length of the array parameter
+  var randIndex = ;
+  // Use the random number made to get an element out of the array
+  var randElement = ;
+  // Return the element
+  return randElement;
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  // Running the function to trigger the prompts and get the users answers back as an object
+  var options = getPasswordOptions();
+  // Array to store password as it's being concatenated 
+  var result = ;
+
+  // Array to store types of characters to include in password
+  var possibleCharacters = ;
+
+  // Array to contain one of each type of chosen character to ensure each will be used
+  var guaranteedCharacters = ;
+
+  // Conditional statement that adds array of special characters into array of possible characters based on user input
+  // Push new random special character to guaranteedCharacters to make at least one of the value is always included 
+  if (options.) {
+
+  }
+
+  // Conditional statement that adds array of numeric characters into array of possible characters based on user input
+  // Push new random special character to guaranteedCharacters to make at least one of the value is always included 
+  if (options.) {
+
+  }
+
+  // Conditional statement that adds array of lowercase characters into array of possible characters based on user input
+  // Push new random lower-cased character to guaranteedCharacters to make at least one of the value is always included 
+  if (options.) {
+
+  }
+
+  // Conditional statement that adds array of uppercase characters into array of possible characters based on user input
+  // Push new random upper-cased character to guaranteedCharacters to make at least one of the value is always included 
+  if (options.) {
+
+  }
+
+  // For loop to iterate over the password length provided from the options object, selecting random indices from the array of possible characters and concatenating those characters into the result variable
+  for (var i = 0; i < options.; i++) {
+    var possibleCharacter = getRandomElement(possibleCharacters);
+  }
+
+  // For loop to iterate the guarenteed characters to overwrite the generated characters
+
+
+  // Join the array to make it a singular string to return 
+  return 
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  // Runs the function that will generate the password
+  var password = generatePassword();
+  // Selects on the HTML where the password is shown
+  var passwordText = document.querySelector('#password');
+  // Makes the value of the element the string generated from the generatePassword function
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
